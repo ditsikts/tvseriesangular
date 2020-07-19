1eb25676c08aba019d0b255370ea79d439caf82f
Dimitris Tsiktsiris <ditsikts@outlook.com>
2020-07-19 15:36:02 +0300

    change event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,404 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  search: string;
+   searchChanged(event){console.log(event.target.value);
+  }
   ngOnInit(): void {
   }
 
 }
+let tvseries = [
+  {
+    "id": 1,
+    "title": "Breaking Bad",
+    "status": "Ended",
+    "imageUrl": null,
+    "seasons": [
+      {
+        "id": 1,
+        "seasonNumber": "Season 1",
+        "dateAired": "2008-01-20",
+        "episodes": 7
+      },
+      {
+        "id": 2,
+        "seasonNumber": "Season 2",
+        "dateAired": "2009-03-08",
+        "episodes": 13
+      },
+      {
+        "id": 3,
+        "seasonNumber": "Season 3",
+        "dateAired": "2010-03-21",
+        "episodes": 13
+      },
+      {
+        "id": 4,
+        "seasonNumber": "Season 4",
+        "dateAired": "2011-07-17",
+        "episodes": 13
+      },
+      {
+        "id": 5,
+        "seasonNumber": "Season 5a",
+        "dateAired": "2012-07-15",
+        "episodes": 8
+      },
+      {
+        "id": 6,
+        "seasonNumber": "Season 5b",
+        "dateAired": "2013-08-11",
+        "episodes": 8
+      }
+    ],
+    "categories": [
+      {
+        "id": 1,
+        "category": "Crime"
+      },
+      {
+        "id": 5,
+        "category": "Drama"
+      },
+      {
+        "id": 6,
+        "category": "Thriller"
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "title": "Altered Carbon",
+    "status": "Ongoing",
+    "imageUrl": null,
+    "seasons": [
+      {
+        "id": 7,
+        "seasonNumber": "Season 1",
+        "dateAired": "2018-02-02",
+        "episodes": 10
+      }
+    ],
+    "categories": [
+      {
+        "id": 2,
+        "category": "Sci-Fi"
+      },
+      {
+        "id": 3,
+        "category": "Action"
+      },
+      {
+        "id": 5,
+        "category": "Drama"
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "title": "Game of Thrones",
+    "status": "Ended",
+    "imageUrl": null,
+    "seasons": [
+      {
+        "id": 8,
+        "seasonNumber": "Season 1",
+        "dateAired": "2011-04-17",
+        "episodes": 10
+      },
+      {
+        "id": 9,
+        "seasonNumber": "Season 2",
+        "dateAired": "2012-04-01",
+        "episodes": 10
+      },
+      {
+        "id": 10,
+        "seasonNumber": "Season 3",
+        "dateAired": "2013-03-31",
+        "episodes": 10
+      },
+      {
+        "id": 11,
+        "seasonNumber": "Season 4",
+        "dateAired": "2014-04-06",
+        "episodes": 10
+      },
+      {
+        "id": 12,
+        "seasonNumber": "Season 5",
+        "dateAired": "2015-04-12",
+        "episodes": 10
+      },
+      {
+        "id": 13,
+        "seasonNumber": "Season 6",
+        "dateAired": "2016-04-24",
+        "episodes": 10
+      },
+      {
+        "id": 14,
+        "seasonNumber": "Season 7",
+        "dateAired": "2017-07-16",
+        "episodes": 7
+      },
+      {
+        "id": 15,
+        "seasonNumber": "Season 8",
+        "dateAired": "2019-04-14",
+        "episodes": 6
+      }
+    ],
+    "categories": [
+      {
+        "id": 3,
+        "category": "Action"
+      },
+      {
+        "id": 5,
+        "category": "Drama"
+      },
+      {
+        "id": 7,
+        "category": "Adventure"
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "title": "Stranger Things",
+    "status": "Ended",
+    "imageUrl": null,
+    "seasons": [
+      {
+        "id": 16,
+        "seasonNumber": "Season 1",
+        "dateAired": "2016-07-15",
+        "episodes": 8
+      },
+      {
+        "id": 17,
+        "seasonNumber": "Season 2",
+        "dateAired": "2017-10-27",
+        "episodes": 9
+      },
+      {
+        "id": 18,
+        "seasonNumber": "Season 3",
+        "dateAired": "2019-04-04",
+        "episodes": 8
+      }
+    ],
+    "categories": [
+      {
+        "id": 4,
+        "category": "Horror"
+      },
+      {
+        "id": 5,
+        "category": "Drama"
+      },
+      {
+        "id": 8,
+        "category": "Fantasy"
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "title": "Sherlock",
+    "status": "Ended",
+    "imageUrl": null,
+    "seasons": [
+      {
+        "id": 19,
+        "seasonNumber": "Season 1",
+        "dateAired": "2010-07-25",
+        "episodes": 3
+      },
+      {
+        "id": 20,
+        "seasonNumber": "Season 2",
+        "dateAired": "2012-01-01",
+        "episodes": 3
+      },
+      {
+        "id": 21,
+        "seasonNumber": "Season 3",
+        "dateAired": "2014-01-01",
+        "episodes": 3
+      },
+      {
+        "id": 22,
+        "seasonNumber": "Special",
+        "dateAired": "2016-01-01",
+        "episodes": 1
+      },
+      {
+        "id": 23,
+        "seasonNumber": "Season 4",
+        "dateAired": "2017-01-01",
+        "episodes": 3
+      }
+    ],
+    "categories": [
+      {
+        "id": 1,
+        "category": "Crime"
+      },
+      {
+        "id": 5,
+        "category": "Drama"
+      },
+      {
+        "id": 9,
+        "category": "Mystery"
+      }
+    ]
+  },
+  {
+    "id": 6,
+    "title": "Dexter",
+    "status": "Ended",
+    "imageUrl": null,
+    "seasons": [
+      {
+        "id": 24,
+        "seasonNumber": "Season 1",
+        "dateAired": "2006-10-01",
+        "episodes": 12
+      },
+      {
+        "id": 25,
+        "seasonNumber": "Season 2",
+        "dateAired": "2007-09-30",
+        "episodes": 12
+      },
+      {
+        "id": 26,
+        "seasonNumber": "Season 3",
+        "dateAired": "2008-09-28",
+        "episodes": 12
+      },
+      {
+        "id": 27,
+        "seasonNumber": "Season 4",
+        "dateAired": "2009-09-27",
+        "episodes": 12
+      },
+      {
+        "id": 28,
+        "seasonNumber": "Season 5",
+        "dateAired": "2010-09-26",
+        "episodes": 12
+      },
+      {
+        "id": 29,
+        "seasonNumber": "Season 6",
+        "dateAired": "2011-10-02",
+        "episodes": 12
+      },
+      {
+        "id": 30,
+        "seasonNumber": "Season 7",
+        "dateAired": "2012-09-30",
+        "episodes": 12
+      },
+      {
+        "id": 31,
+        "seasonNumber": "Season 8",
+        "dateAired": "2013-06-30",
+        "episodes": 12
+      }
+    ],
+    "categories": [
+      {
+        "id": 1,
+        "category": "Crime"
+      },
+      {
+        "id": 5,
+        "category": "Drama"
+      },
+      {
+        "id": 9,
+        "category": "Mystery"
+      }
+    ]
+  },
+  {
+    "id": 7,
+    "title": "Fargo",
+    "status": "Ended",
+    "imageUrl": null,
+    "seasons": [
+      {
+        "id": 32,
+        "seasonNumber": "Season 1",
+        "dateAired": "2014-04-15",
+        "episodes": 10
+      },
+      {
+        "id": 33,
+        "seasonNumber": "Season 2",
+        "dateAired": "2015-10-12",
+        "episodes": 10
+      },
+      {
+        "id": 34,
+        "seasonNumber": "Season 3",
+        "dateAired": "2017-04-19",
+        "episodes": 10
+      }
+    ],
+    "categories": [
+      {
+        "id": 1,
+        "category": "Crime"
+      },
+      {
+        "id": 5,
+        "category": "Drama"
+      },
+      {
+        "id": 6,
+        "category": "Thriller"
+      }
+    ]
+  },
+  {
+    "id": 8,
+    "title": "FlashForward",
+    "status": "Cancelled",
+    "imageUrl": null,
+    "seasons": [
+      {
+        "id": 35,
+        "seasonNumber": "Part 1",
+        "dateAired": "2009-09-24",
+        "episodes": 10
+      },
+      {
+        "id": 36,
+        "seasonNumber": "Part 2",
+        "dateAired": "2010-03-18",
+        "episodes": 12
+      }
+    ],
+    "categories": [
+      {
+        "id": 2,
+        "category": "Sci-Fi"
+      },
+      {
+        "id": 5,
+        "category": "Drama"
+      },
+      {
+        "id": 9,
+        "category": "Mystery"
+      }
+    ]
+  }
+]
